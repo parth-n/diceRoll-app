de732632eaa5b4f59c26322c037ed2d0ae94deae
Parth Nepalia <parth.nepalia@gmail.com>
2023-12-06 14:56:04 +0530

    commit 3

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(this.outputtext, {super.key});
+  const StyledText(this.outText, {super.key});
 
-  String outputtext;
+  final String outText;
   @override
-  Widget build(BuildContext context) {  
-    return const Text(
-      text,
-      style: TextStyle(color: Colors.white, fontSize: 28.0),
+  Widget build(BuildContext context) {
+    return Text(
+      outText,
+      style: const TextStyle(color: Colors.white, fontSize: 28.0),
     );
   }
 }
