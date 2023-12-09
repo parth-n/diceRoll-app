40b6f7f9bb2d05fe307fbe971092ab73a5ac9042
Parth Nepalia <parth.nepalia@gmail.com>
2023-12-09 11:00:46 +0530

    app completed:stateful class and state set

---
+++ lib/dice_roller.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  //var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
+
+  void rollDice() {
+    // the argument passed in nextInt() is the max value to be generated but the value itself is excluded.
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(
+            //   top: 20,
+            // ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Baatcheet'),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:project_1/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(const Color.fromARGB(255, 102, 37, 213),
-            const Color.fromARGB(255, 63, 11, 152)),
+        body: GradientContainer(Color.fromARGB(255, 102, 37, 213),
+            Color.fromARGB(255, 63, 11, 152)),
       ),
     ),
   );
